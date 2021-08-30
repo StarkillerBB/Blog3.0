0c08b89184d24761267544a90e37d34e178f91b7
Benjamin Marius Lank <benj007d@elevcampus.dk>
2021-08-30 09:52:42 +0200

    ConnectionString Created

--- Blog3.0/Program.cs
+++ Blog3.0/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,10 @@ namespace Blog3._0
 {
     class Program
     {
+
+        static readonly string strcon = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+        static readonly SqlConnection con = new SqlConnection(strcon);
+
         static void Main(string[] args)
         {
 
